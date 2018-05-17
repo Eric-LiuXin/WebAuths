94be55f8eb4dd9f8c2fbfcd45fc6da7d3e4ac986
Eric-LiuXin <liuxin_coding@sina.com>
2018-05-17 15:31:01 +0800

    Create user and userauths table
    Add user and userauths model, and magriate to db.

--- DWebSite/webauths/models.py
+++ DWebSite/webauths/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length = 200)
+    type = models.CharField(max_length = 20, default = 'trainee')
+
+class UserAuths(models.Model):
+    user_id = models.PositiveIntegerField(blank=False)
+    identity_type = models.CharField(max_length = 20, default = 'account')
+    indentifer = models.CharField(max_length = 20)
+    credential = models.CharField(max_length = 20)
