bf6dbb2741a7401a5ef16cc1b29d2743a8fcc724
Eric-LiuXin <liuxin_coding@sina.com>
2018-05-17 17:20:24 +0800

    loacl save

--- DWebSite/webauths/models.py
+++ DWebSite/webauths/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length = 200)
-    type = models.CharField(max_length = 20, default = 'trainee')
+    type = models.CharField(max_length = 200, default = 'trainee')
 
 class UserAuths(models.Model):
     user_id = models.PositiveIntegerField(blank=False)
