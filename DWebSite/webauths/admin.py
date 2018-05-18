6f8a6c7f5044432a4f886dc87a73769e8398cfc5
Eric-LiuXin <liuxin_coding@sina.com>
2018-05-18 14:03:36 +0800

    Add test data
    Add user to db by admin.

--- DWebSite/webauths/admin.py
+++ DWebSite/webauths/admin.py
@@ -4,5 +4,11 @@ from django.contrib import admin
 
 from .models import User, UserAuths
 
-admin.site.register(User)
-admin.site.register(UserAuths)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'type')
+
+class UserAuthsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'indentifer')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(UserAuths, UserAuthsAdmin)
--- DWebSite/webauths/models.py
+++ DWebSite/webauths/models.py
@@ -6,8 +6,14 @@ class User(models.Model):
     name = models.CharField(max_length = 200)
     type = models.CharField(max_length = 200, default = 'trainee')
 
+    def __unicode__(self):
+        return self.name
+
 class UserAuths(models.Model):
     user_id = models.PositiveIntegerField(blank=False)
     identity_type = models.CharField(max_length = 20, default = 'account')
     indentifer = models.CharField(max_length = 20)
     credential = models.CharField(max_length = 20)
+
+    def __unicode__(self):
+        return self.user_id
